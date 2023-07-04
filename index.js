e86bf02be802b6b15aa7aa6af8d28f54855d9282
leamr10 <leamr10@gmail.com>
2023-07-04 16:20:48 -0400

    added inquirer

---
+++ index.js
@@ -0,0 +1,60 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown')
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'project',
+    },
+    {
+        type: 'input',
+        message: 'Write a description',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'How to install',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Usage',
+        name: 'usage',
+    },
+    {
+        type: 'list',
+        message: 'License',
+        name: 'license',
+        choices: ['MIT', 'Apache 2.0', 'BSD 3', 'GPL 3.0', 'None'],
+    },
+    {
+        type: 'input',
+        message: 'Contributing',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'How to test?',
+        name: 'testing',
+    },
+    {
+        type: 'input',
+        message: 'Email',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'Username',
+        name: 'username',
+    }
+])
+
+.then(data => {
+    fs.writeFile('README.md', generateMarkdown(data), (err) => {
+        if (err) console.log(err)
+        console.log('Success!')
+    })
+})
